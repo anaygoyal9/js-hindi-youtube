ca6c01d9d0226b4ea023ad8bd2b66890a345bba3
Anay Goyal <anaygoyal9@gmail.com>
2025-09-09 00:53:02 +0530

    add basics

---
+++ basics/data_types.js
@@ -0,0 +1,19 @@
+"use strict" // we'll use the mordern js version code but nowdays no need of it as it is by defalult
+//alert(3+3) // not in node.js, it is in browsers
+
+// console.log("hi") console.log("world"); // wrong
+
+//numbers=> 2 ot pow 53
+//bigint
+//string
+//null => stand alone value => intentionally nothing
+// undefined=> not assigned or absence
+// boolean = true/false
+//symbol
+//object
+var name = "hitesh";
+console.log(typeof "hitesh"); // string
+console.log(typeof name); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
---
+++ basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 123456;
+let accountEmail = "hitest@google.com";
+var accountPassword = "12345";
+accountAddress  = 'Jaipur';
+let accountState
+// accountId = 2;
+console.log(accountId);
+// table function to print the table of entities
+console.table([accountId,accountEmail,accountPassword,accountAddress,accountState]) 
